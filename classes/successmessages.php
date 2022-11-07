2e30db2c0088eb59bf2ce46600012e80264ccf24
juanpabedoyav <bedoyavascojuanpablo@gmail.com>
2022-11-07 13:59:20 -0500

    add

--- classes/successmessages.php
+++ classes/successmessages.php
@@ -1,6 +1,4 @@
 <?php 
-
-
 class SuccessMessages{
     const SUCCESS_ADMIN_NEWCATEGORY_EXIST = '236ba46e8d55889af2cf6901cd34ed26';
     const SUCCESS_SINGUP_NEWUSER = 'dfdfsfasdfdsaf78988998978fasdfasdfasd'; 
