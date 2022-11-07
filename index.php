245a4ae55cfce072d62440718de3f2eb03240b23
juanpabedoyav <bedoyavascojuanpablo@gmail.com>
2022-11-07 14:16:09 -0500

    add user model

--- index.php
+++ index.php
@@ -9,6 +9,8 @@ error_log( "start app" );
 require_once 'libs/db.php';
 require_once 'classes/errormessages.php';
 require_once 'classes/successmessages.php';
+require_once 'classes/errormessages.php';
+require_once 'classes/successmessages.php';
 require_once 'libs/controller.php';
 require_once 'libs/model.php';
 require_once 'libs/view.php';
